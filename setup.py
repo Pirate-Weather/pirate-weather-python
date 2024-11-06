6966eeabafb26eb0c2ed073bbc198e8519afd7df
Kev <cloneofghosts@users.noreply.github.com>
2024-11-06 13:10:11 -0500

    PyPI Publishing Workflow (#9)

--- setup.py
+++ setup.py
@@ -8,16 +8,16 @@ __version__ = "0.0.9"
 with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md")) as f:
     README = f.read()
 
-repo_url = "https://github.com/bakobako/pirate-weather-python"
+repo_url = "https://github.com/Pirate-Weather/pirate-weather-python"
 setup(
     version=__version__,
     name="pirate-weather-python",
     packages=find_packages(),
     install_requires=["requests==2.32.3", "pytz==2024.2", "aiohttp==3.10.10"],
     description="The Pirate Weather API wrapper",
-    long_description="View on github",
-    author="bakobako",
-    author_email="adam.bako@keboola.com",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Pirate-Weather",
     url=repo_url,
     download_url=f"{repo_url}/archive/{__version__}.tar.gz",
     license="GPLv3 License",
